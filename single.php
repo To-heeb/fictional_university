c109cd4fefef95c1892544f6d29a76209701ede2
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-05-07 23:56:46 +0100

    custom post type event have been added

--- single.php
+++ single.php
@@ -1,9 +1,26 @@
-<?php
-while (have_posts()) {
+    <?php
+    get_header();
+    while (have_posts()) {
 
-    the_post(); ?>
+        the_post(); ?>
 
-    <h2><?php the_title(); ?></h2>
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title(); ?></h1>
+                <div class="page-banner__intro">
+                    <p><?php echo "PLEASE REPLACE ME LATER" ?></p>
+                </div>
+            </div>
+        </div>
+        <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?= site_url('/blog') ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?= the_author_posts_link() ?> on <?= the_time('n-j-y') ?> in <?php echo get_the_category_list(', ') ?></span>
+                </p>
+            </div>
+            <div class="generic-content"><?php the_content(); ?></div>
+        </div>
+    <?php }
 
-    <?php the_content(); ?>
-<?php }
+    get_footer();
